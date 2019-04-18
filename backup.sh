b0d338eaedc694d15573feeeadec52a5e52f661c
Matthias Schobner <matthias.schobner@me.com>
2019-04-18 18:06:00 +0200

    refactor compatibility Strato server: Add support for db host and absolute path not work with Strato scp

--- backup.sh
+++ backup.sh
@@ -25,51 +25,49 @@ datestr=$(date +"%Y_%m_%d")
 timestr=$(date +"%H_%M_%S")
 
 # set backup directory name
-backupname=$datestr"_"$timestr
+backupname="${datestr}_${timestr}"
 
 
 # set connection details
-connection_details="$ssh_user@$ssh_host"
+ssh_connection="${ssh_user}@${ssh_host}"
+mysql_connection="-h ${db_host} -u ${db_user} -p'${db_password}' ${db_name}"
 
 
-ssh $connection_details << EOF
-    set -xe
+# set remote files
+tar_file="${backups_path}${backupname}_files.tar.gz"
+sql_file="${backups_path}${backupname}_database.sql"
+
+
+ssh ${ssh_connection} << EOF
+    # create remote backup directory
+    mkdir -p ${backups_path} && echo 'remote backup directory created'
 
     # clean up remote backup directory
-    rm -rfv ${backups_path} && echo 'remote backup directory cleaned up'
-
-    # create remote backup directory
-    mkdir -p ${backups_path}/${backupname} && echo 'remote backup directory created'
+    rm -rfv ${backups_path}* && echo 'remote backup directory cleaned up'
 
     # create remote files backup
-    tar cfpz ${backups_path}/${backupname}/${backupname}_files.tar.gz ${website_path} && echo 'remote files backup completed'
+    tar cfpz ${tar_file} ${website_path} && echo 'remote files backup completed'
 
     # create remote database backup
-    mysqldump -u ${db_user} -p'${db_password}' ${db_name} --result-file=${backups_path}/${backupname}/${backupname}_database.sql && echo 'remote database backup completed'
+    mysqldump ${mysql_connection} --result-file=${sql_file} && echo 'remote database backup completed'
 EOF
 
 
 # create local backup directory
-mkdir 'backups_'$1 && echo 'created local backup directory "backups_'$1'"'
+mkdir "backups_${1}" && echo 'created local backup directory "backups_'$1'"'
 
 
 # download remote files backup
-scp $connection_details:$backups_path/$backupname/$backupname"_files.tar.gz" "backups_"$1"/"$backupname"_files.tar.gz" && echo downloaded remote files backup
+scp ${ssh_connection}:${tar_file} "backups_${1}/${backupname}_files.tar.gz" && echo downloaded remote files backup
 
 # download remote database backup
-scp $connection_details:$backups_path/$backupname/$backupname"_database.sql" "backups_"$1"/"$backupname"_database.sql" && echo downloaded remote database backup
+scp ${ssh_connection}:${sql_file} "backups_${1}/${backupname}_database.sql" && echo downloaded remote database backup
 
 
-ssh $connection_details << EOF
-    set -xe
+ssh ${ssh_connection} << EOF
+    rm ${tar_file} && echo 'deleted remote files backup'
 
-    # delete remote files backup
-    rm $backups_path/$backupname/$backupname'_files.tar.gz' && echo 'deleted remote files backup'
+    rm ${sql_file} && echo 'deleted remote database backup'
 
-    # delete remote database backup
-    rm $backups_path/$backupname/$backupname'_database.sql' && echo 'deleted remote database backup'
-
-    # delete remote backup directory
-    rm -rf $backups_path/$backupname && echo 'deleted remote backup directory'
-
+    rm -rfv ${backups_path}* && echo 'remote backup directory cleaned up'
 EOF
